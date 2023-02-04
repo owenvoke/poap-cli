5fa27aec5d840d9c1fce4af4477a7236f569827f
Owen Voke <development@voke.dev>
2023-02-04 22:23:02 +0000

    feat: remove old method of authentication

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Support\ServiceProvider;
 use OwenVoke\POAP\Client;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,30 +15,16 @@ class AppServiceProvider extends ServiceProvider
         renderUsing($this->app->make(OutputInterface::class));
 
         $this->app->bind(Client::class, function ($app) {
-            $apiToken = Cache::remember(config('poap.api_key_cache_id'), now()->addHours(12), function () {
-                $response = Http::asJson()->post(sprintf('%s/oauth/token', config('services.poap.auth_url')), [
-                    'audience' => config('services.poap.audience'),
-                    'client_id' => config('services.poap.client_id'),
-                    'client_secret' => config('services.poap.client_secret'),
-                    'grant_type' => 'client_credentials',
-                ]);
-
-                throw_unless(
-                    $response->ok(),
-                    'Failed to authenticate with the POAP Auth0 API, please check your credentials'
-                );
-
-                throw_unless(
-                    isset($response['access_token'], $response['expires_in']) && $response['access_token'] !== null,
-                    'Invalid Bearer token returned from the Auth0 API'
-                );
-
-                return $response['access_token'];
-            });
+            /** @var Repository $config */
+            $config = $app->make(Repository::class);
 
             $client = new Client();
 
-            $client->authenticate($apiToken, null, Client::AUTH_ACCESS_TOKEN);
+            $client->authenticate(
+                $config->get('services.poap.token'),
+                null,
+                Client::AUTH_ACCESS_TOKEN
+            );
 
             return $client;
         });
--- config/services.php
+++ config/services.php
@@ -1,17 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
 
     'poap' => [
-
-        'auth_url' => env('POAP_API_AUTH_URL', 'https://poapauth.auth0.com/oauth/token'),
-
-        'client_id' => env('POAP_API_CLIENT_ID'),
-        'client_secret' => env('POAP_API_CLIENT_SECRET'),
-        'audience' => env('POAP_API_AUDIENCE'),
-
-        'api_key_cache_id' => env('POAP_API_CACHE_ID', 'poap.api_token'),
-
+        'token' => env('POAP_API_TOKEN'),
     ],
 
 ];
