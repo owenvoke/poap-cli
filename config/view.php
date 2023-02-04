72cc1e0ef7db613fa62aef3c7665807d40026ff1
Owen Voke <development@voke.dev>
2023-02-04 21:36:55 +0000

    docs: update changelog

--- config/view.php
+++ config/view.php
@@ -1,12 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'paths' => [
         resource_path('views'),
     ],
 
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'compiled' => sys_get_temp_dir(),
 ];
