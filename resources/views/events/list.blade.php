499fd8ebbe553b3410729fd2295f05d12df475c2
Owen Voke <development@voke.dev>
2023-05-19 16:29:09 +0100

    chore: update `events:list` to include link

--- resources/views/events/list.blade.php
+++ resources/views/events/list.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-1 ml-1">
     <ul>
         @foreach($events as $event)
-            <li><strong>{{ preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F]|\xED[\xA0-\xBF].|\xEF\xBF[\xBE\xBF]/', '\xEF\xBF\xBD', $event['name']) }}</strong> (#{{ $event['id'] }})</li>
+            <li><strong>{{ preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F]|\xED[\xA0-\xBF].|\xEF\xBF[\xBE\xBF]/', '\xEF\xBF\xBD', $event['name']) }}</strong> (<a href="https://poap.gallery/event/{{ $event['id'] }}">#{{ $event['id'] }}</a>)</li>
         @endforeach
     </ul>
 </div>
